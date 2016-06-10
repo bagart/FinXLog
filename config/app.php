25bb29b1cb2d8420ecfdedabd02d497c033cfa8d
bag <bagart@list.ru>
2016-06-10 08:23:45 +0300

    config requirements

--- config/app.php
+++ config/app.php
@@ -6,3 +6,6 @@ putenv('FINXLOG_ROOT_PATH=' . realpath(__DIR__ . '/..') . '/');
 
 $dot_env->required('FINXLOG_QUOTATION_SERVER_ADDRESS')->notEmpty();
 $dot_env->required('FINXLOG_QUOTATION_SERVER_PORT')->notEmpty();
+$dot_env->required('FINXLOG_ELASTICO_PARAM')->notEmpty();
+
+assert(json_decode(getenv('FINXLOG_ELASTICO_PARAM')));
